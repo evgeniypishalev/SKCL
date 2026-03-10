d03bc4e8e239a724a103215ec0b2dd178dda51fa
evgeniypishalev <dfgsigma977@gmail.com>
2026-03-10 23:50:11 +0200

    Update routes.js

--- public/js/routes.js
+++ public/js/routes.js
@@ -4,12 +4,14 @@ import Roulette from './pages/Roulette.js';
 import Admin from './pages/Admin.js';
 import Manage from './pages/manage.js';
 import ListPacks from './pages/ListPacks.js';
+import Upcoming from './pages/Upcoming.js'; // <-- Добавь это
 
 export default [
-    { path: '/', component: List },
-    { path: '/leaderboard', component: Leaderboard },
-    { path: '/roulette', component: Roulette },
-    { path: '/admin', component: Admin },
-    { path: '/manage', component: Manage },
-    { path: '/packs', component: ListPacks },
+  { path: '/', component: List },
+  { path: '/leaderboard', component: Leaderboard },
+  { path: '/roulette', component: Roulette },
+  { path: '/admin', component: Admin },
+  { path: '/manage', component: Manage },
+  { path: '/packs', component: ListPacks },
+  { path: '/upcoming', component: Upcoming }, // <-- И это
 ];
